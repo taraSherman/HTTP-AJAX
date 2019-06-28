4f72c1e7ded937c1a556c0feb774f0c96ad9320c
Tara Sherman <taradelin@gmail.com>
2019-06-27 21:29:01 -0400

    Fixed Home.js

--- friends/src/components/Home.js
+++ friends/src/components/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default HomePage(props) {
+export default function(props) {
     const randomFriend = () => {
         const randomId = Math.floor(Math.random() * props.friends.length)
         props.history.push(`/friend/${randomId}`)
